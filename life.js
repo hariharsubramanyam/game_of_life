61aa2d750ece59d24c043d5dc80f8f5f06b8da2e
Harihar Subramanyam <hahasubbu@hotmail.com>
2014-09-04 18:37:09 -0400

    Added specification for life.js, which represents the game logic for the Game of Life.

---
+++ life.js
@@ -0,0 +1,26 @@
+/**
+ * Game logic for the Game of Life.
+ * @constructor
+ */
+var Life = function(){ 
+};
+
+/**
+ * Clears the entire game board and sets the given cells as live cells.
+ * @param {Array} live_cells - Array of objects indicating the live cells. 
+ *                                For each obj in live_cells, (obj.x, obj.y) should be the location of a live cell. 
+ */
+Life.prototype.reset = function(live_cells) {
+};
+
+/**
+ * Takes one step in the Game of Life.
+ * @return {Array} Array which indicates the changes to the Game of Life after taking the step.
+ *                 Each element of the array is an object (call it obj) where:
+ *                     obj.state {String} = "Live" if the cell has just become alive due to this step.
+ *                     obj.state {String} = "Dead" if the cell has just died due to this step.
+ *                     obj.x {Number} = The x-coordinate of the cell.
+ *                     obj.y {Number} = The y-coordinate of the cell.
+ */
+Life.prototype.step = function() {
+};
